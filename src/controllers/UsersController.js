c1f141c0626016a4768e2227a562dc761e4791b6
Neves-Rafael <nevesrafael.dev@gmail.com>
2023-12-01 20:47:13 -0300

    :heavy_plus_sign: Add KNEX.js Dependency

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,6 +1,7 @@
-const { hash } = require("bcryptjs");
+const { hash, compare } = require("bcryptjs");
 const AppError = require("../utils/AppError");
 const sqliteConnection = require("../database/sqlite");
+const { use } = require("../routes");
 
 class UsersController {
   async create(request, response) {
@@ -27,7 +28,7 @@ class UsersController {
   }
 
   async update(request, response) {
-    const { name, email } = request.body;
+    const { name, email, password, old_password } = request.body;
     const { id } = request.params;
 
     const database = await sqliteConnection();
@@ -46,17 +47,31 @@ class UsersController {
       throw new AppError("This E-mail already exist!");
     }
 
-    user.name = name;
-    user.email = email;
+    user.name = name ?? user.name;
+    user.email = email ?? user.email;
 
+    if(password && !old_password){
+      throw new AppError("You need put a old Password For change a new Password!")
+    }
+
+    if(password && old_password){
+      const checkOldPassword = await compare(old_password, user.password)
+
+      if(!checkOldPassword) {
+        throw new AppError("The old Password is not correct")
+      }
+
+      user.password = await hash(password, 8);
+    }
 
     await database.run(`
       UPDATE users SET
       name = ?,
       email = ?,
-      updated_at = ?
+      password = ?,
+      updated_at = DATETIME ('now')
       WHERE id = ?`,
-      [user.name, user.email, new Date(), id]
+      [user.name, user.email, user.password, id]
       )
 
     return response.status(200).json();
