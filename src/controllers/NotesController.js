72fee0fa4166efa5a4eb078ba373b210a8f97352
Neves-Rafael <nevesrafael.dev@gmail.com>
2023-12-03 17:39:21 -0300

    :sparkles: Feat: Notes listing feature by user

--- src/controllers/NotesController.js
+++ src/controllers/NotesController.js
@@ -52,6 +52,14 @@ class NotesController {
 
     return response.json();
   }
+
+  async index(request, response) {
+    const { user_id } = request.query;
+
+    const notes = await knex("notes").where({ user_id }).orderBy("title");
+
+    return response.json(notes)
+  }
 }
 
 module.exports = NotesController;
--- src/routes/notes.routes.js
+++ src/routes/notes.routes.js
@@ -4,6 +4,8 @@ const NotesController = require("../controllers/NotesController");
 const notesRoutes = Router();
 
 const notesController = new NotesController();
+
+notesRoutes.get("/", notesController.index);
 notesRoutes.post("/:user_id", notesController.create);
 notesRoutes.get("/:id", notesController.show);
 notesRoutes.delete("/:id", notesController.delete);
