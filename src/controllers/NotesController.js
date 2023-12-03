a2caafa0f172c89ed4e7f210be6eb5b61f1438fe
Neves-Rafael <nevesrafael.dev@gmail.com>
2023-12-03 20:03:45 -0300

    :sparkles: Feat: Search tags by words

--- src/controllers/NotesController.js
+++ src/controllers/NotesController.js
@@ -54,11 +54,27 @@ class NotesController {
   }
 
   async index(request, response) {
-    const { user_id } = request.query;
+    const { title, user_id, tags } = request.query;
 
-    const notes = await knex("notes").where({ user_id }).orderBy("title");
+    let notes;
+    if (tags) {
+      const filterTags = tags.split(",").map((tag) => tag.trim());
 
-    return response.json(notes)
+      notes = await knex("tags")
+        .select(["notes.id", "notes.title", "notes.user_id"])
+        .where("notes.user_id", user_id)
+        .whereLike("notes.title", `%${title}%`)
+        .whereIn("name", filterTags)
+        .innerJoin("notes", "notes.id", "tags.note_id")
+        .orderBy("notes.title");
+    } else {
+      notes = await knex("notes")
+        .where({ user_id })
+        .whereLike("title", `%${title}%`)
+        .orderBy("title");
+    }
+
+    return response.json(notes);
   }
 }
 
