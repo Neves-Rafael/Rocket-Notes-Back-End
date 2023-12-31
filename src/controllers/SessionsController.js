5631a4aaa10d7d53f0edf3d9d0b8ff1637fc908d
Neves-Rafael <nevesrafael.dev@gmail.com>
2023-12-31 14:57:18 -0300

    :sparkles: Feat password validation

--- src/controllers/SessionsController.js
+++ src/controllers/SessionsController.js
@@ -1,5 +1,6 @@
 const knex = require("../database/knex");
 const AppError = require("../utils/AppError");
+const { compare } = require("bcryptjs");
 
 class SessionController{
     async create(request, response){
@@ -8,6 +9,12 @@ class SessionController{
         const user = await knex("users").where({email}).first();
 
         if(!user){
+            throw new AppError("E-mail ou senha incorretos", 401);
+        }
+
+        const passwordMatched = await compare(password, user.password);
+
+        if(!passwordMatched){
             throw new AppError("E-mail ou senha incorretos", 401);
         }
 
