f1e9bcf9523b4ac2cbd588511724c5e68b913e21
Neves-Rafael <nevesrafael.dev@gmail.com>
2023-12-30 21:13:36 -0300

    :sparkles: Feat validating registered user

--- src/controllers/SessionsController.js
+++ src/controllers/SessionsController.js
@@ -1,7 +1,17 @@
+const knex = require("../database/knex");
+const AppError = require("../utils/AppError");
+
 class SessionController{
     async create(request, response){
         const {email, password} = request.body;
-        return response.json({email, password});
+
+        const user = await knex("users").where({email}).first();
+
+        if(!user){
+            throw new AppError("E-mail ou senha incorretos", 401);
+        }
+
+        return response.json(user);
     }
 }
 module.exports = SessionController;
