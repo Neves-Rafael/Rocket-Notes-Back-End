2b6ffb953bc073f564e898887d797888ee58f3f2
Neves-Rafael <nevesrafael.dev@gmail.com>
2023-12-31 15:11:28 -0300

    :sparkles: Feat generating user token

--- src/controllers/SessionsController.js
+++ src/controllers/SessionsController.js
@@ -1,6 +1,8 @@
 const knex = require("../database/knex");
 const AppError = require("../utils/AppError");
 const { compare } = require("bcryptjs");
+const authConfig = require("../configs/auth");
+const {  sign } = require("jsonwebtoken")
 
 class SessionController{
     async create(request, response){
@@ -18,9 +20,13 @@ class SessionController{
             throw new AppError("E-mail ou senha incorretos", 401);
         }
 
+        const { secret, expiresIn } = authConfig.jwt;
+        const token = sign({}, secret, {
+            subject: String(user.id),
+            expiresIn
+        })
 
-        
-        return response.json(user);
+        return response.json({user, token});
     }
 }
 module.exports = SessionController;
