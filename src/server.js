e7f20cc8b703419eef4362b1fc0523e36bfb4c51
Neves-Rafael <nevesrafael.dev@gmail.com>
2023-12-31 21:46:33 -0300

    :sparkles: Feat searching files

--- src/server.js
+++ src/server.js
@@ -4,12 +4,15 @@ const migrationsRun = require("./database/sqlite/migrations");
 const AppError = require("./utils/AppError");
 const express = require("express");
 const routes = require("./routes");
+const uploadConfig = require("./configs/upload");
 
 migrationsRun();
 
 const app = express();
 app.use(express.json());
 app.use(routes);
+
+app.use("/files", express.static(uploadConfig.UPLOADS_FOLDER))
 
 app.use((error, request, response, next) => {
   if (error instanceof AppError) {
