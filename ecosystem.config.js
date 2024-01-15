ebc5177f0aed834e0f511f361076bcf5fb45da24
Neves-Rafael <nevesrafael.dev@gmail.com>
2024-01-14 23:09:58 -0300

    :sparkles: Feat pm2 config.js

---
+++ ecosystem.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  apps: [
+    {
+      name: "app",
+      script: "./app.js",
+      env: {
+        NODE_ENV: "development",
+      },
+      env_production: {
+        NODE_ENV: "production",
+      },
+    },
+  ],
+};
