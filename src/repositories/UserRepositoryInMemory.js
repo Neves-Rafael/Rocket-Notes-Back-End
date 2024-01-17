e0fe367f81ab4f6d29bec2c1fa3807b2027f3fd7
Neves-Rafael <nevesrafael.dev@gmail.com>
2024-01-16 21:59:05 -0300

    :sparkles: Feat create userRepositoryInMemory

---
+++ src/repositories/UserRepositoryInMemory.js
@@ -0,0 +1,19 @@
+class UserRepositoryInMemory {
+  users = [];
+  async create({ name, email, password }) {
+    const user = {
+      id: Math.floor(Math.random() * 1000) + 1,
+      email,
+      name,
+      password,
+    };
+
+    this.users.push(user);
+
+    return user;
+  }
+
+  async findByEmail(email) {
+      return this.users.find(user => user.email === email)
+  }
+}
--- src/services/userCreateService.spec.js
+++ src/services/userCreateService.spec.js
@@ -1,4 +1,5 @@
 const UserCreateService = require("./UserCreateService");
+const UserRepositoryInMemory = require("../repositories/UserRepositoryInMemory");
 
 it("user should be create", () => {
   const user = {
@@ -7,7 +8,8 @@ it("user should be create", () => {
     password: "123",
   };
 
-  const userCreateService = new UserCreateService();
+  const userRepositoryInMemory = new UserRepositoryInMemory();
+  const userCreateService = new UserCreateService(userRepositoryInMemory);
   const userCreated = userCreateService.execute(user);
 
   expect(userCreated).toHaveProperty("id");
