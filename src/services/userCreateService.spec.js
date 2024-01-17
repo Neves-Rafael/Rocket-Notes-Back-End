91b72ba4663ac87ee3055fd993fbcf78249b4789
Neves-Rafael <nevesrafael.dev@gmail.com>
2024-01-17 17:15:53 -0300

    :sparkles: Feat add describe in jest test

--- src/services/userCreateService.spec.js
+++ src/services/userCreateService.spec.js
@@ -1,16 +1,18 @@
 const UserCreateService = require("./UserCreateService");
 const UserRepositoryInMemory = require("../repositories/UserRepositoryInMemory");
 
-it("user should be create", async () => {
-  const user = {
-    name: "User Test",
-    email: "user@test.com",
-    password: "123",
-  };
+describe("UserCreateService", () => {
+  it("user should be create", async () => {
+    const user = {
+      name: "User Test",
+      email: "user@test.com",
+      password: "123",
+    };
 
-  const userRepositoryInMemory = new UserRepositoryInMemory();
-  const userCreateService = new UserCreateService(userRepositoryInMemory);
-  const userCreated = await userCreateService.execute(user);
+    const userRepositoryInMemory = new UserRepositoryInMemory();
+    const userCreateService = new UserCreateService(userRepositoryInMemory);
+    const userCreated = await userCreateService.execute(user);
 
-  expect(userCreated).toHaveProperty("id");
+    expect(userCreated).toHaveProperty("id");
+  });
 });
