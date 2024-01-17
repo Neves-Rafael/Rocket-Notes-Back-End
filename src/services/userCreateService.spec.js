e90710182753ccb94c4770d4a06eae6896dae4bf
Neves-Rafael <nevesrafael.dev@gmail.com>
2024-01-17 17:55:03 -0300

    :recycle: Refactor with beforeEach

--- src/services/userCreateService.spec.js
+++ src/services/userCreateService.spec.js
@@ -3,6 +3,14 @@ const UserRepositoryInMemory = require("../repositories/UserRepositoryInMemory")
 const AppError = require("../utils/AppError");
 
 describe("UserCreateService", () => {
+  let userRepositoryInMemory = null;
+  let userCreateService = null;
+
+  beforeEach(() => {
+    userRepositoryInMemory = new UserRepositoryInMemory();
+    userCreateService = new UserCreateService(userRepositoryInMemory);
+  });
+
   it("user should be create", async () => {
     const user = {
       name: "User Test",
@@ -10,8 +18,6 @@ describe("UserCreateService", () => {
       password: "123",
     };
 
-    const userRepositoryInMemory = new UserRepositoryInMemory();
-    const userCreateService = new UserCreateService(userRepositoryInMemory);
     const userCreated = await userCreateService.execute(user);
 
     expect(userCreated).toHaveProperty("id");
@@ -29,9 +35,6 @@ describe("UserCreateService", () => {
       email: "user@test.com",
       password: "123",
     };
-
-    const userRepository = new UserRepositoryInMemory();
-    const userCreateService = new UserCreateService(userRepository);
 
     await userCreateService.execute(user1);
     await expect(userCreateService.execute(user2)).rejects.toEqual(
