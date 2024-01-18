dd47046afbbf03cfee6cc37f87ba327309612209
Neves-Rafael <nevesrafael.dev@gmail.com>
2024-01-17 23:23:50 -0300

    :recycle: Refactor userAvatarController

--- src/controllers/UserAvatarController.js
+++ src/controllers/UserAvatarController.js
@@ -1,38 +1,38 @@
-
 const AppError = require("../utils/AppError");
 const DiskStorage = require("../providers/DiskStorage");
 const UserRepository = require("../repositories/UserRepository");
 const knex = require("../database/knex");
 
-class UserAvatarController{
-    async update(request, response){
-        const user_id = request.user.id;
-        const avatarFilename = request.file.filename;
-        const userRepository = new UserRepository();
+class UserAvatarController {
+  async update(request, response) {
+    const user_id = request.user.id;
+    let avatarFilename;
+    const userRepository = new UserRepository();
 
-        if(!avatarFilename){
-            throw new AppError("File not found", 404);
-        }
-
-        const user = await userRepository.findById(user_id);
-        // const user = await knex("users").where({id: user_id}).first();
-
-        if(!user){
-            throw new AppError("Unauthenticated user", 404);
-        }
-
-        if(user.avatar){
-            await userRepository.deleteAvatar(user.avatar);
-
-        }
-
-        const filename = await userRepository.saveAvatar(avatarFilename);
-        user.avatar = filename;
-
-        userRepository.updateAvatar({user_id, user});
-
-        return response.json(user);
+    if (!request.file) {
+      throw new AppError("File not found");
+    } else {
+      avatarFilename = request.file.filename;
     }
+
+    const user = await userRepository.findById(user_id);
+    // const user = await knex("users").where({id: user_id}).first();
+
+    if (!user) {
+      throw new AppError("Unauthenticated user", 404);
+    }
+
+    if (user.avatar) {
+      await userRepository.deleteAvatar(user.avatar);
+    }
+
+    const filename = await userRepository.saveAvatar(avatarFilename);
+    user.avatar = filename;
+
+    userRepository.updateAvatar({ user_id, user });
+
+    return response.json(user);
+  }
 }
 
 module.exports = UserAvatarController;
