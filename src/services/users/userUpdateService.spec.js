9c0a7e1078635fb6e905ca73c676431b1e124a8f
Neves-Rafael <nevesrafael.dev@gmail.com>
2024-01-19 14:34:27 -0300

    :test_tube: Test add updateService

--- src/services/users/userUpdateService.spec.js
+++ src/services/users/userUpdateService.spec.js
@@ -33,8 +33,94 @@ describe("userUpdateService", () => {
     );
 
     console.log(userUpdated);
-    expect(userUpdatedResult.name).toEqual(userUpdated.name);
-    expect(userUpdatedResult.email).toEqual(userUpdated.email);
-    expect(userUpdatedResult.password).toEqual(userUpdated.password);
+    await expect(userUpdatedResult.name).toEqual(userUpdated.name);
+    await expect(userUpdatedResult.email).toEqual(userUpdated.email);
+    await expect(userUpdatedResult.password).toEqual(userUpdated.password);
+  });
+
+  it("Atualizar usuário com senha errada", async () => {
+    const user = {
+      name: "User Test",
+      email: "user@test.com",
+      password: "123",
+    };
+
+    const userCreate = await userRepositoryInMemory.create(user);
+
+    await expect(
+      userUpdateService.execute({
+        user_id: userCreate.id,
+        name: "User Test Updated",
+        email: "user@test.com",
+        old_password: "999",
+        password: "456",
+      })
+    ).rejects.toEqual(new AppError("The old Password is not correct"));
+  });
+
+  it("Atualizar usuário com email existente", async () => {
+    const user = {
+      name: "User Test",
+      email: "user@test.com",
+      password: "123",
+    };
+    const user2 = {
+      name: "User Test",
+      email: "user2@test.com",
+      password: "123",
+    };
+
+    const userCreate = await userRepositoryInMemory.create(user);
+    const userCreate2 = await userRepositoryInMemory.create(user2);
+
+    await expect(
+      userUpdateService.execute({
+        user_id: userCreate.id,
+        name: "User Test Updated",
+        email: "user2@test.com",
+        old_password: "123",
+        password: "456",
+      })
+    ).rejects.toEqual(new AppError("This E-mail already exist!"));
+  });
+
+  it("Atualizar usuário sem informar senha", async () => {
+    const user = {
+      name: "User Test",
+      email: "user@test.com",
+      password: "123",
+    };
+
+    const userCreate = await userRepositoryInMemory.create(user);
+
+    await expect(
+      userUpdateService.execute({
+        user_id: userCreate.id,
+        name: "User Test Updated",
+        email: "user@test.com",
+        password: "456",
+      })
+    ).rejects.toEqual(
+      new AppError("You need put a old Password For change a new Password!")
+    );
+  });
+
+  it("Atualizar usuário inválido", async () => {
+    const user = {
+      name: "User Test",
+      email: "user@test.com",
+      password: "123",
+    };
+
+    const userCreate = await userRepositoryInMemory.create(user);
+
+    await expect(
+      userUpdateService.execute({
+        user_id: "5",
+        name: "User Test Updated",
+        email: "user@test.com",
+        password: "456",
+      })
+    ).rejects.toEqual(new AppError("User not found!"));
   });
 });
