5f34dc82ec24f26af22dcf7608a53dc3403c1e3f
Neves-Rafael <nevesrafael.dev@gmail.com>
2024-01-21 21:07:28 -0300

    :test_tube: Test update noteDeleteService

--- src/repositories/NotesRepositoryInMemory.js
+++ src/repositories/NotesRepositoryInMemory.js
@@ -17,12 +17,11 @@ class NotesRepositoryInMemory {
   async show(id) {}
 
   async delete(id) {
+    //verify result delete in notes
     const index = this.notes.findIndex((note) => note.user_id === id);
     if (index !== -1) {
       this.notes.splice(index, 1);
     }
-
-    console.log(this.notes);
 
     if (this.notes[0] && this.notes[0].user_id === id) {
       return this.notes[0].user_id;
